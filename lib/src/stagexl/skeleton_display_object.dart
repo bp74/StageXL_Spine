6dea54f4736be8166b49a57eb67fc58cb31755cd
Bernhard <bernhard.robert.pichler@gmail.com>
2014-12-22 15:44:42 +0100

    Enabled FFD for canvas renderer
    It is slow but better than nothing

--- lib/src/stagexl/skeleton_display_object.dart
+++ lib/src/stagexl/skeleton_display_object.dart
@@ -37,17 +37,22 @@ class SkeletonDisplayObject extends DisplayObject {
 
   //-----------------------------------------------------------------------------------------------
 
-  static Float32List _xyList = new Float32List(256);
+  static Float32List _xyList = new Float32List(4096);
   static Int16List _indexList = new Int16List.fromList([0, 1, 2, 0, 2, 3]);
 
   void _renderWebGL(RenderState renderState) {
 
-    var renderContext = renderState.renderContext;
-    var globalMatrix = renderState.globalMatrix;
-    var blendMode = renderState.globalBlendMode;
-
+    RenderContext renderContext = renderState.renderContext;
     RenderContextWebGL renderContextWebGL = renderContext;
     RenderProgramMesh renderProgramMesh = renderContextWebGL.renderProgramMesh;
+    Matrix globalMatrix = renderState.globalMatrix;
+    BlendMode globalBlendMode = renderState.globalBlendMode;
+    num globalAlpha = renderState.globalAlpha;
+
+    Float32List xyList = _xyList;
+    Float32List uvList = new Float32List(0);
+    Int16List indexList = new Int16List(0);
+    List<Slot> drawOrder = skeleton.drawOrder;
 
     num skeletonX = skeleton.x;
     num skeletonY = skeleton.y;
@@ -59,14 +64,6 @@ class SkeletonDisplayObject extends DisplayObject {
     renderContextWebGL.flush();
     renderContextWebGL.activateRenderProgram(renderProgramMesh);
     renderProgramMesh.globalMatrix = globalMatrix;
-
-    Float32List xyList = _xyList;
-    Float32List uvList = new Float32List(0);
-    Int16List indexList = new Int16List(0);
-
-    //---------------------------------------------------
-
-    List<Slot> drawOrder = skeleton.drawOrder;
 
     for (var i = 0; i < drawOrder.length; i++) {
 
@@ -86,61 +83,48 @@ class SkeletonDisplayObject extends DisplayObject {
 
       if (attachment is RegionAttachment) {
 
-        RegionAttachment regionAttachment = attachment;
-
-        regionAttachment.computeWorldVertices(skeletonX, skeletonY, bone, xyList);
-        uvList = regionAttachment.uvs;
+        uvList = attachment.uvs;
         indexList = _indexList;
         vertexCount = 4;
         indexCount = 6;
 
-        attachmentR = regionAttachment.r;
-        attachmentG = regionAttachment.g;
-        attachmentB = regionAttachment.b;
-        attachmentA = regionAttachment.a;
-        renderTexture = regionAttachment.bitmapData.renderTexture;
+        attachment.computeWorldVertices(skeletonX, skeletonY, bone, xyList);
+
+        attachmentR = attachment.r;
+        attachmentG = attachment.g;
+        attachmentB = attachment.b;
+        attachmentA = attachment.a;
+        renderTexture = attachment.bitmapData.renderTexture;
 
       } else if (attachment is MeshAttachment) {
 
-        MeshAttachment meshAttachment = attachment;
-
-        uvList = meshAttachment.uvs;
-        indexList = meshAttachment.triangles;
+        uvList = attachment.uvs;
+        indexList = attachment.triangles;
         vertexCount = uvList.length >> 1;
         indexCount = indexList.length;
 
-        if (xyList.length < uvList.length) {
-          xyList = _xyList = new Float32List(uvList.length);
-        }
+        attachment.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
 
-        meshAttachment.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
-
-        attachmentR = meshAttachment.r;
-        attachmentG = meshAttachment.g;
-        attachmentB = meshAttachment.b;
-        attachmentA = meshAttachment.a;
-        renderTexture = meshAttachment.bitmapData.renderTexture;
+        attachmentR = attachment.r;
+        attachmentG = attachment.g;
+        attachmentB = attachment.b;
+        attachmentA = attachment.a;
+        renderTexture = attachment.bitmapData.renderTexture;
 
       } else if (attachment is SkinnedMeshAttachment) {
 
-        SkinnedMeshAttachment skinnedMesh = attachment;
-
-        uvList = skinnedMesh.uvs;
-        indexList = skinnedMesh.triangles;
+        uvList = attachment.uvs;
+        indexList = attachment.triangles;
         vertexCount = uvList.length >> 1;
         indexCount = indexList.length;
 
-        if (xyList.length < uvList.length) {
-          xyList = _xyList = new Float32List(uvList.length);
-        }
+        attachment.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
 
-        skinnedMesh.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
-
-        attachmentR = skinnedMesh.r;
-        attachmentG = skinnedMesh.g;
-        attachmentB = skinnedMesh.b;
-        attachmentA = skinnedMesh.a;
-        renderTexture = skinnedMesh.bitmapData.renderTexture;
+        attachmentR = attachment.r;
+        attachmentG = attachment.g;
+        attachmentB = attachment.b;
+        attachmentA = attachment.a;
+        renderTexture = attachment.bitmapData.renderTexture;
       }
 
       //---------------------------------------------------
@@ -152,9 +136,15 @@ class SkeletonDisplayObject extends DisplayObject {
         num bb = attachmentB * skeletonB * slot.b;
         num aa = attachmentA * skeletonA * slot.a;
 
+        BlendMode blendMode = slot.data.additiveBlending ? BlendMode.ADD : globalBlendMode;
+
         renderContextWebGL.activateRenderTexture(renderTexture);
-        renderContextWebGL.activateBlendMode(slot.data.additiveBlending ? BlendMode.ADD : blendMode);
-        renderProgramMesh.renderMesh(indexCount, indexList, vertexCount, xyList, uvList, rr, gg, bb, aa);
+        renderContextWebGL.activateBlendMode(blendMode);
+
+        renderProgramMesh.renderMesh(
+            indexCount, indexList,
+            vertexCount, xyList, uvList,
+            rr, gg, bb, aa);
       }
     }
   }
@@ -167,28 +157,85 @@ class SkeletonDisplayObject extends DisplayObject {
     Matrix globalMatrix = renderState.globalMatrix.clone();
     BlendMode globalBlendMode = renderState.globalBlendMode;
     num globalAlpha = renderState.globalAlpha;
-
     Matrix tmpMatrix = new Matrix.fromIdentity();
     RenderState tmpRenderState = new RenderState(renderContext);
+
+    Float32List xyList = _xyList;
+    Float32List uvList = new Float32List(0);
+    Int16List indexList = new Int16List(0);
     List<Slot> drawOrder = skeleton.drawOrder;
 
-    for (int i = 0; i < drawOrder.length; i++) {
+    num skeletonX = skeleton.x;
+    num skeletonY = skeleton.y;
+    num skeletonA = skeleton.a;
+
+    for (var i = 0; i < drawOrder.length; i++) {
 
       Slot slot = drawOrder[i];
       Bone bone = slot.bone;
       Attachment attachment = slot.attachment;
+      RenderTextureQuad renderTextureQuad = null;
+      RenderTexture renderTexture = null;
+      BlendMode blendMode = globalBlendMode;
+      int vertexCount = 0;
+      int indexCount = 0;
+      num alpha = 0.0;
+
+      num attachmentR = 0.0;
+      num attachmentG = 0.0;
+      num attachmentB = 0.0;
+      num attachmentA = 0.0;
+
+      //---------------------------------------------------
 
       if (attachment is RegionAttachment) {
 
-        RegionAttachment regionAttachment = attachment;
-        RenderTextureQuad renderTextureQuad = regionAttachment.bitmapData.renderTextureQuad;
-        BlendMode blendMode = slot.data.additiveBlending ? BlendMode.ADD : globalBlendMode;
-        num alpha = globalAlpha * skeleton.a * regionAttachment.a * slot.a;
+        blendMode = slot.data.additiveBlending ? BlendMode.ADD : globalBlendMode;
+        alpha = globalAlpha * skeletonA * attachment.a * slot.a;
 
-        tmpMatrix.copyFromAndConcat(regionAttachment.matrix, bone.worldMatrix);
+        tmpMatrix.copyFrom(attachment.matrix);
+        tmpMatrix.translate(skeletonX, skeletonY);
+        tmpMatrix.concat(bone.worldMatrix);
         tmpMatrix.concat(globalMatrix);
+
         tmpRenderState.reset(tmpMatrix, alpha, blendMode);
-        tmpRenderState.renderQuad(renderTextureQuad);
+        tmpRenderState.renderQuad(attachment.bitmapData.renderTextureQuad);
+
+      } else if (attachment is MeshAttachment) {
+
+        blendMode = slot.data.additiveBlending ? BlendMode.ADD : globalBlendMode;
+        alpha = globalAlpha * skeletonA * attachment.a * slot.a;
+
+        uvList = attachment.uvs;
+        indexList = attachment.triangles;
+        vertexCount = uvList.length >> 1;
+        indexCount = indexList.length;
+
+        attachment.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
+
+        tmpRenderState.reset(globalMatrix, alpha, blendMode);
+        renderContext.renderMesh(
+            tmpRenderState, attachment.bitmapData.renderTexture,
+            indexCount, indexList,
+            vertexCount, xyList, uvList);
+
+      } else if (attachment is SkinnedMeshAttachment) {
+
+        blendMode = slot.data.additiveBlending ? BlendMode.ADD : globalBlendMode;
+        alpha = globalAlpha * skeletonA * attachment.a * slot.a;
+
+        uvList = attachment.uvs;
+        indexList = attachment.triangles;
+        vertexCount = uvList.length >> 1;
+        indexCount = indexList.length;
+
+        attachment.computeWorldVertices(skeletonX, skeletonY, slot, xyList);
+
+        tmpRenderState.reset(globalMatrix, alpha, blendMode);
+        renderContext.renderMesh(
+            tmpRenderState, attachment.bitmapData.renderTexture,
+            indexCount, indexList,
+            vertexCount, xyList, uvList);
       }
     }
   }
