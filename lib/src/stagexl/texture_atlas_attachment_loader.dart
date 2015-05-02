9c70946f62191ce456c2b40b4fea0f2fc51a9ef5
Bernhard <bernhard.robert.pichler@gmail.com>
2015-05-02 13:48:59 +0200

    Added optional namePrefix for BitmapDatas in texture atlas

--- lib/src/stagexl/texture_atlas_attachment_loader.dart
+++ lib/src/stagexl/texture_atlas_attachment_loader.dart
@@ -3,21 +3,22 @@ part of stagexl_spine;
 class TextureAtlasAttachmentLoader implements AttachmentLoader {
 
   final TextureAtlas textureAtlas;
+  final String namePrefix;
 
-  TextureAtlasAttachmentLoader(this.textureAtlas) {
+  TextureAtlasAttachmentLoader(this.textureAtlas, [this.namePrefix = ""]) {
     if (textureAtlas == null) throw new ArgumentError("textureAtlas cannot be null.");
   }
 
   RegionAttachment newRegionAttachment(Skin skin, String name, String path) {
-    return new RegionAttachment(name, textureAtlas.getBitmapData(path));
+    return new RegionAttachment(name, textureAtlas.getBitmapData(namePrefix + path));
   }
 
   MeshAttachment newMeshAttachment(Skin skin, String name, String path) {
-    return new MeshAttachment(name, textureAtlas.getBitmapData(path));
+    return new MeshAttachment(name, textureAtlas.getBitmapData(namePrefix + path));
   }
 
   SkinnedMeshAttachment newSkinnedMeshAttachment(Skin skin, String name, String path) {
-    return new SkinnedMeshAttachment(name, textureAtlas.getBitmapData(path));
+    return new SkinnedMeshAttachment(name, textureAtlas.getBitmapData(namePrefix + path));
   }
 
   BoundingBoxAttachment newBoundingBoxAttachment(Skin skin, String name) {
