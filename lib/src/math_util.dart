a6909d1c63b7b6d7826d275cd9b913013abd323b
Bernhard <bernhard.robert.pichler@gmail.com>
2016-10-30 10:17:50 +0100

    Add documentation to new math utilities

--- lib/src/math_util.dart
+++ lib/src/math_util.dart
@@ -1,16 +1,22 @@
 part of stagexl_spine;
 
+/// multiplier for degrees to radians conversion
 double _deg2rad = math.PI / 180.0;
+
+/// multiplier for radians to degrees conversion
 double _rad2deg = 180.0 / math.PI;
 
+/// calculate cos based on degrees
 double _cosDeg(double deg) => math.cos(_deg2rad * deg);
+
+/// calculate sin based on degrees
 double _sinDeg(double deg) => math.sin(_deg2rad * deg);
 
+/// convert radians to degrees
 double _toDeg(double rad) => _rad2deg * rad;
+
+/// convert degrees to radians
 double _toRad(double deg) => _deg2rad * deg;
 
-/// Wrap within -180 and 180 degrees.
-double _wrapRotation(double deg) {
-  return deg - 360.0 * (deg / 360.0).round();
-  //return deg - (16384 - (16384.499999999996 - deg / 360).toInt()) * 360;
-}
+/// Wrap within -180 degrees and 180 degrees
+double _wrapRotation(double deg) => deg - 360.0 * (deg / 360.0).round();
